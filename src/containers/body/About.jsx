01b289038852498ad759117168e0d12f1c113674
eydrianJS <adrian.olszowski@wp.pl>
2019-04-03 20:59:22 +0200

    ikony do o about

--- src/containers/body/About.jsx
+++ src/containers/body/About.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const About = () => {
     return ( 
         <section id="about">
-            o mnie
+            <div className="parent-cechy">
+                <div className="cecha">
+                    <i class="mdi mdi-language-javascript"></i>                    
+                </div>
+            </div>
+            <div className="parent-cechy">
+                <div className="cecha">
+                <i class="mdi mdi-react"></i>
+                </div>
+            </div>
+            <div className="parent-cechy">
+                <div className="cecha">
+                <i class="mdi mdi-language-python"></i>
+                </div>
+            </div>
+            <div className="parent-cechy">
+                <div className="cecha">
+                    <i class="mdi mdi-nodejs"></i>
+                </div>
+            </div>
         </section> 
     );
 }
