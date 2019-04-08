50794207e3e0040ac149e29eb1b31cd648d8da7f
Adrian Olszowski <olszowski@europa-tu.local>
2019-04-08 15:38:38 +0200

    sekcja work

--- src/containers/body/Work.jsx
+++ src/containers/body/Work.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import Hexa from './Hexa';
+
 
 const Work = () => {
     return ( 
         <section id="work">
-            <Hexa/>
+            <div className="body-work">
+                <div className="project-border">
+                    <div className="image-border">
+                    </div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque quae fugiat error perferendis, libero velit ratione! Quam nisi maiores asperiores, consequatur, facere dolorum, hic harum non delectus dolores iste! Tempore.
+                </div>
+                <div className="project-border">
+                    <div className="image-border">
+                    </div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque officia debitis dolorum unde voluptatum iste veniam quibusdam distinctio, ex velit rerum veritatis ipsum ut libero illo accusamus illum laboriosam explicabo?
+                </div>
+                <div className="project-border">
+                    <div className="image-border">
+                    </div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugit nesciunt vitae, cupiditate cum harum consequuntur deserunt, voluptatibus atque nihil non odit eum quae et! Libero eius neque consequatur ea!
+                </div>
+            </div>
         </section>
      );
 }
