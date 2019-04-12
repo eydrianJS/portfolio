799298f1322ac6938ecb2f5fc10c37fbf143a7c8
Adrian Olszowski <olszowski@europa-tu.local>
2019-04-12 15:02:55 +0200

    przechodzenie do poszczegolnych sekcji

--- src/containers/Body.jsx
+++ src/containers/Body.jsx
@@ -1,26 +1,43 @@
-import React, { Component } from 'react';
-import Navbar from './body/Navbar'
-import About from './body/about/About'
-import Work from './body/work/Work'
-import Blog from './body/blog/Blog'
-import Contact from './body/contact/Contact';
+import React, { Component } from "react";
+import $, { easing } from "jquery";
+import Navbar from "./body/Navbar";
+import About from "./body/about/About";
+import Work from "./body/work/Work";
+import Blog from "./body/blog/Blog";
+import Contact from "./body/contact/Contact";
+require('jquery.easing');
 
 class Body extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-        <div className="body">
-            <Navbar clickHome={this.props.clickHome} />
-            <About />
-            <Work />
-            <Blog />
-            <Contact />
-        </div>   
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // console.log($('a.js-scroll-trigger[href*="#"]:not([href="#"])'));
+        $("div.js-scroll-trigger").click(function() {
+            var target = $("#" + $(this).attr("sect"));
+            if (target.length) {
+                $('html, body').animate({
+                  scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+              }
+        
+        });
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <Navbar clickHome={this.props.clickHome} />
+        <About />
+        <Work />
+        <Blog />
+        <Contact />
+      </div>
+    );
+  }
 }
- 
+
 export default Body;
