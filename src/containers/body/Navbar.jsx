d2be24c73aa444480b4569efb62c57889a8d7b0e
Adrian Olszowski <olszowski@europa-tu.local>
2019-04-17 08:49:38 +0200

    menu na telefon

--- src/containers/body/Navbar.jsx
+++ src/containers/body/Navbar.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-const Navbar = (props) => {
+const Navbar = props => {
+
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
-      <nav className="menu-flex">
+    <>
+      <nav className={openMenu ? "menu-flex"  : "menu-flex-none" }>
         <div className="nav">
-            <div onClick={props.clickHome}>HOME</div>
-            <div className="js-scroll-trigger" dest="about">O MNIE</div>
-            <div className="js-scroll-trigger" dest="work">PRACA</div>
-            <div className="js-scroll-trigger" dest="blog">BLOGI</div>
-            <div className="js-scroll-trigger" dest="contact">KONTAKT</div>
+          <div onClick={props.clickHome}>HOME</div>
+          <div
+            className="js-scroll-trigger"
+            dest="about"
+            onClick={() => setOpenMenu(false)}
+          >
+            O MNIE
+          </div>
+          <div
+            className="js-scroll-trigger"
+            dest="work"
+            onClick={() => setOpenMenu(false)}
+          >
+            PRACA
+          </div>
+          <div
+            className="js-scroll-trigger"
+            dest="blog"
+            onClick={() => setOpenMenu(false)}
+          >
+            BLOGI
+          </div>
+          <div
+            className="js-scroll-trigger"
+            dest="contact"
+            onClick={() => setOpenMenu(false)}
+          >
+            KONTAKT
+          </div>
         </div>
       </nav>
+      <div className="menu">
+        {openMenu ? (
+          <i className="mdi mdi-mixer" onClick={() => setOpenMenu(!openMenu)} />
+        ) : (
+          <i className="mdi mdi-menu" onClick={() => setOpenMenu(!openMenu)} />
+        )}
+      </div>
+    </>
   );
 };
 
