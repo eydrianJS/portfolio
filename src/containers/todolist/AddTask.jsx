e1d1eb4d54ad2414236c4e25ffcb5615c8d673d6
eydrianJS <adrian.olszowski@wp.pl>
2019-04-24 09:23:08 +0200

    dodawanie tasków

--- src/containers/todolist/AddTask.jsx
+++ src/containers/todolist/AddTask.jsx
@@ -1,15 +1,53 @@
-import React, {Component} from 'react';
+import React, { useState } from "react";
 
-class AddTask extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+const AddTask = props => {
+  const minDate = new Date().toISOString().slice(0, 10);
+  let maxDate = minDate.slice(0, 4) * 1 + 2;
+  maxDate = maxDate + minDate.slice(4,10)
+  const [text, setText] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [date, setDate] = useState(minDate);
+  const handleClick = () => {
+    const obj = {
+        text: text,
+        date: date,
+        important: checked,
     }
-    render() { 
-        return ( 
-            <div>Task</div> 
-         );
+    const addTask = props.add(obj)
+    if(addTask) {
+        setText("")
+        setChecked("")
+        setDate(minDate)
     }
-}
- 
+  }
+
+  return (
+    <div className="form">
+      <input
+        type="text"
+        placeholder="Dodaj zadanie"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+        id="important"
+      />
+      <label htmlFor="important">Priorytet</label> <br />
+      <label htmlFor="duedate">Do kiedy zrobić</label>
+      <input
+        type="date"
+        value={date}
+        min={minDate}
+        max={maxDate}
+        onChange={e => setDate(e.target.value)}
+      />
+      <br />
+      <button onClick={handleClick}>DODAJ</button>
+    </div>
+  );
+};
+
 export default AddTask;
--- src/containers/todolist/ToDoList.jsx
+++ src/containers/todolist/ToDoList.jsx
@@ -1,95 +1,115 @@
-import React, {Component} from 'react';
-import AddTask from './AddTask'
-import TaskList from './TaskList';
+import React, { Component } from "react";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
 
 class ToDoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            tasks: [
-                {
-                    id: 0,
-                    text: 'adasdas',
-                    date: '2018-02-15',
-                    important: true,
-                    active: true,
-                    finishDate: null,
-                },
-                {
-                    id: 1,
-                    text: 'dsfdsfffffffffffffff',
-                    date: '2019-05-13',
-                    important: true,
-                    active: true,
-                    finishDate: null,
-                },
-                {
-                    id: 2,
-                    text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-                    date: '2018-01-11',
-                    important: false,
-                    active: true,
-                    finishDate: null,
-                },
-                {
-                    id: 3,
-                    text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-                    date: '2018-01-11',
-                    important: false,
-                    active: true,
-                    finishDate: null,
-                },
-                {
-                    id: 4,
-                    text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-                    date: '2018-01-11',
-                    important: false,
-                    active: true,
-                    finishDate: null,
-                },
-                {
-                    id: 5,
-                    text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-                    date: '2018-01-11',
-                    important: false,
-                    active: true,
-                    finishDate: null,
-                },
-            ]
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.counter = 6;
+    this.state = {
+      tasks: [
+        {
+          id: 0,
+          text: "adasdas",
+          date: "2018-02-15",
+          important: true,
+          active: true,
+          finishDate: null
+        },
+        {
+          id: 1,
+          text: "dsfdsfffffffffffffff",
+          date: "2019-05-13",
+          important: true,
+          active: true,
+          finishDate: null
+        },
+        {
+          id: 2,
+          text: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+          date: "2018-01-11",
+          important: false,
+          active: true,
+          finishDate: null
+        },
+        {
+          id: 3,
+          text: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+          date: "2018-01-11",
+          important: false,
+          active: true,
+          finishDate: null
+        },
+        {
+          id: 4,
+          text: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+          date: "2018-01-11",
+          important: false,
+          active: true,
+          finishDate: null
+        },
+        {
+          id: 5,
+          text: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+          date: "2018-01-11",
+          important: false,
+          active: true,
+          finishDate: null
+        }
+      ]
+    };
+  }
 
-    deleteTask = id => {
-        let tasks = [...this.state.tasks]
-        tasks = tasks.filter(item => item.id !== id);
-        this.setState({
-            tasks
-        })
-    }
+  deleteTask = id => {
+    let tasks = [...this.state.tasks];
+    tasks = tasks.filter(item => item.id !== id);
+    this.setState({
+      tasks
+    });
+  };
 
-    changeTaskStatus = id => {
-        const tasks = [...this.state.tasks]
-        tasks.forEach(item => {
-            if (item.id === id) {
-                item.active = false;
-                item.finishDate = new Date().getTime();
-            }
-        })
-        this.setState({
-            tasks
-        })
-    }
+  changeTaskStatus = id => {
+    const tasks = [...this.state.tasks];
+    tasks.forEach(item => {
+      if (item.id === id) {
+        item.active = false;
+        item.finishDate = new Date().getTime();
+      }
+    });
+    this.setState({
+      tasks
+    });
+  };
 
-    render() { 
-        return ( 
-            <div className="todoapp">
-            TODO APP
-            <AddTask />
-            <TaskList tasks={this.state.tasks} delete={this.deleteTask} change={this.changeTaskStatus}/>
-        </div>
-         );
-    }
+  addtask = ({text, date, important} ) => {
+    const task = {
+      id: this.counter,
+      text: text,
+      date: date,
+      important: important,
+      active: true,
+      finishDate: null
+    };
+    this.counter++;
+    this.setState( prevState => ({
+        tasks: [...prevState.tasks, task]
+    }))
+    return true;
+  };
+
+  render() {
+    return (
+      <div className="todoapp">
+        <h1>TODO APP</h1>
+        <AddTask add={this.addtask} />
+        <TaskList
+          tasks={this.state.tasks}
+          delete={this.deleteTask}
+          change={this.changeTaskStatus}
+        />
+      </div>
+    );
+  }
 }
- 
-export default ToDoList;
 
+export default ToDoList;
