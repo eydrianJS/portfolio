620b64d11589542f467653bf8ee4c6d5c978a587
eydrianJS <adrian.olszowski@wp.pl>
2019-04-24 19:55:16 +0200

    Updates

---
+++ precache-manifest.8be8c4479a760eaed363bc8314399107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1b0e9ad6ceb957af3da",
+    "url": "/portfolio/static/js/runtime~main.89080ce3.js"
+  },
+  {
+    "revision": "3a8557fd92f86fb63cb2",
+    "url": "/portfolio/static/js/main.803946c5.chunk.js"
+  },
+  {
+    "revision": "49063ef61bf8b6bc477e",
+    "url": "/portfolio/static/js/4.c2a7ac52.chunk.js"
+  },
+  {
+    "revision": "b3f3b1d2110e12afec14",
+    "url": "/portfolio/static/js/3.d506063e.chunk.js"
+  },
+  {
+    "revision": "04c8f4e441ce40e51b2a",
+    "url": "/portfolio/static/js/2.247e6e48.chunk.js"
+  },
+  {
+    "revision": "b5fa565b2478ef80ceafdca3d1acbc34",
+    "url": "/portfolio/index.html"
+  }
+];
