723875876edd32c8c8b724269798e527e4109a60
eydrianJS <adrian.olszowski@wp.pl>
2019-04-25 14:39:11 +0200

    odnosniki do footer

--- src/containers/portfolio/Footer.jsx
+++ src/containers/portfolio/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => {
     return ( 
         <footer>
-            <i className="mdi mdi-facebook-box"></i>
-            <i className="mdi mdi-linkedin-box"></i>
-            <i className="mdi mdi-github-circle"></i>
-            <i className="mdi mdi-nodejs"></i>
+            <a href="https://www.facebook.com/adrian.olszowski10" target="_blank"><i className="mdi mdi-facebook-box"></i></a>
+            <a href="https://www.linkedin.com/in/adrian-olszowski-944518138/" target="_blank"><i className="mdi mdi-linkedin-box"></i></a>
+            <a href="https://github.com/eydrianJS" target="_blank"><i className="mdi mdi-github-circle"></i></a>
+            <a href="https://www.facebook.com/adrian.olszowski10" target="_blank"><i className="mdi mdi-nodejs"></i></a>
         </footer>
      );
 }
