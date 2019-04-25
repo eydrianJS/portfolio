b40519fcd3c8b131cb15bbb3bf5098838d0add7a
eydrianJS <adrian.olszowski@wp.pl>
2019-04-25 09:51:42 +0200

    dodanie strzelki do scrollowania

--- src/containers/portfolio/body/blog/Blog.jsx
+++ src/containers/portfolio/body/blog/Blog.jsx
@@ -6,7 +6,7 @@ const Blog = (props) => {
         <blockquote>Moje projekty<span></span></blockquote>
         <img id="office" src="{{site.baseurl}}/../img/biurko.jpg" />
         <img id="building" src="{{site.baseurl}}/../img/pjona.jpg" onClick={props.openTodo}/>
-        <div id="box"/>
+        <div id="box"><i className="mdi mdi-chevron-triple-down"/></div>
     </section> 
     );
 }
