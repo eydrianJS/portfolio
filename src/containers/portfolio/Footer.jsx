b173962ad25186b11447115223c8e20c0f0e8a8c
stud-A230B07 <stud-A230B07@EDU-WSB.local>
2019-10-20 11:16:49 +0200

    adding li

--- src/containers/portfolio/Footer.jsx
+++ src/containers/portfolio/Footer.jsx
@@ -3,10 +3,21 @@ import React from 'react';
 const Footer = () => {
     return ( 
         <footer>
-            <a href="https://www.facebook.com/adrian.olszowski10" target="_blank"><i className="mdi mdi-facebook-box"></i></a>
-            <a href="https://www.linkedin.com/in/adrian-olszowski-944518138/" target="_blank"><i className="mdi mdi-linkedin-box"></i></a>
-            <a href="https://github.com/eydrianJS" target="_blank"><i className="mdi mdi-github-circle"></i></a>
-            <a href="https://www.facebook.com/adrian.olszowski10" target="_blank"><i className="mdi mdi-nodejs"></i></a>
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/adrian.olszowski10" target="_blank"><i className="mdi mdi-facebook-box"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/adrian-olszowski-944518138/" target="_blank"><i className="mdi mdi-linkedin-box"></i></a>
+                </li>
+                <li>
+                    <a href="https://github.com/eydrianJS" target="_blank"><i className="mdi mdi-github-circle"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/adrian.olszowski10" target="_blank"><i className="mdi mdi-nodejs"></i></a>
+                </li>
+            </ul>
+
         </footer>
      );
 }
